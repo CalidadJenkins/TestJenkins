7aaa94f21fdccdb36138fbf38ff822dd63970a0c
Ngonzalez693 <nicogonto@gmail.com>
2024-05-10 20:05:45 -0500

    metodo request

--- src/main/java/co/com/calidad/controller/Confirmacion.java
+++ src/main/java/co/com/calidad/controller/Confirmacion.java
@@ -1,14 +1,14 @@
 package co.com.calidad.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Confirmacion {
 
-	@GetMapping("/confir")
+    @RequestMapping(value = "/confir", method = RequestMethod.GET)
     public String confirmacion() {
         return "Su mensaje ha sido recibido";
     }
-	
 }
